ce2a905b7e66308069e34fb930851a6286f5aef0
simonmuia <simonmuia38@gmail.com>
2023-01-13 12:38:34 +0300

    Added Routes to Landing Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello YouTube</h1>
-    </div>
+    <BrowserRouter>
+    <Box sx={{backgroundColor:'#000'}}>
+      <Navbar/>
+      <Routes>
+        <Route path="/" exact element={<Feed/>}/>
+        <Route path="/video/:id" exact element={<VideoDetail/>}/>
+        <Route path="/channel/:id" exact element={<ChannelDetail/>}/>
+        <Route path="/search/:searchTerm" exact element={<SearchFeed/>}/>
+      </Routes>
+    </Box>
+    </BrowserRouter>
   );
 }
 
