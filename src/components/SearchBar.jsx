3618975c1393b53ad9a6cf06c76cbdc1c3581e7f
simonmuia <simonmuia38@gmail.com>
2023-01-13 14:55:37 +0300

    Added Category Sidebar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import {Search} from "@mui/icons-material";
 
