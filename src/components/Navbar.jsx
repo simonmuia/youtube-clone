8a0e1ec0fd15309b0aae4797b27a53754af3abd4
simonmuia <simonmuia38@gmail.com>
2023-01-13 13:11:10 +0300

    Added Brand Icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { logo } from "../utils/constants";
 
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    
+  </Stack>
+);
 
-export default Navbar
+export default Navbar;
