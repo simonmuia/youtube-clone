7425f451afce2a413217ac64a6333fd2112fed56
simonmuia <simonmuia38@gmail.com>
2023-01-13 14:53:05 +0300

    Added Category Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import {Box, Stack, Typography} from '@mui/material';
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { format } from "date-fns/esm";
+import { SideBar } from "./index";
 
-function Feed() {
+const Feed = () => {
+  const year = format(new Date(),'yyyy');
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar/>
+        <Typography 
+        className="copyright"
+        variant="body2"
+        sx={{
+          color:'#fff'
+        }}
+        
+        >
+          Copyright &copy; {year} Simon Muia&trade;
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
